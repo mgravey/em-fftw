944c1525c6b7805fd8ea8803ae2ea1d2c85505f9
Mathieu Gravey <mathieu.gravey@unil.ch>
2017-08-09 14:33:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Em-FFTW
+A small script to compile fftw/ffwf with emscripten
+EM_PATH : the prefix for the library need to be setted
---
+++ make.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+if [ -z ${EM_PATH+x} ]; then 
+	echo "EM_PATH is unset"; 
+	export EM_PATH="$(PWD)/test"
+else 
+	echo "EM_PATH is set to '$EM_PATH'"; 
+fi
+
+wget "http://www.fftw.org/fftw-3.3.6-pl2.tar.gz"
+tar -xf fftw-3.3.6-pl2.tar.gz
+cd fftw-3.3.6-pl2
+emconfigure ./configure --disable-fortran --enable-float --enable-double --enable-threads --prefix=$EM_PATH CFLAGS="-s USE_PTHREADS=1" CXXFLAGS="-s USE_PTHREADS=1"  LDFLAGS="-s USE_PTHREADS=1"  #--with-combined-threads
+emmake make -j
+emmake make install
