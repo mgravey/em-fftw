065326a82b4ffec9c0b27c26e0751eb89deff15d
Mathieu Gravey <gravey.mathieu@gmail.com>
2018-07-21 23:30:31 +0200

    minor update, single thread

--- make.sh
+++ make.sh
@@ -10,6 +10,6 @@ fi
 wget "http://www.fftw.org/fftw-3.3.6-pl2.tar.gz"
 tar -xf fftw-3.3.6-pl2.tar.gz
 cd fftw-3.3.6-pl2
-emconfigure ./configure --disable-fortran --enable-float --enable-double --enable-threads --prefix=$EM_PATH CFLAGS="-s USE_PTHREADS=1" CXXFLAGS="-s USE_PTHREADS=1"  LDFLAGS="-s USE_PTHREADS=1"  #--with-combined-threads
+emconfigure ./configure --disable-fortran --enable-float --prefix=$EM_PATH CFLAGS="-s WASM=1" CXXFLAGS="-s WASM=1"  LDFLAGS="-s WASM=1"  #--with-combined-threads
 emmake make -j
 emmake make install
